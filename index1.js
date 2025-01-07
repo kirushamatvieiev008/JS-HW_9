c58ffaed895b0e5e397406eae862e470cf22ab41
Кірюша <mkiril087@gmail.com>
2025-01-07 17:40:25 +0200

    п

---
+++ index1.js
@@ -0,0 +1,40 @@
+// const x = [`mango`, `ajax`, `bird`, `Andrew`];
+// // console.log(x.length);
+
+
+
+
+
+// const logItems = function(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(`element num: ${i + 1}, element value: ${array[i]}`);
+        
+//     }
+    
+    
+//     return
+// };
+
+// logItems(x);
+
+// task 3
+
+const y = prompt(`enter one sentance`);
+let c = y.split(` `);
+// console.log(c.length);
+let w = [];
+
+
+const findLongestWord = function(string) {
+    for (let i = 0; i < string.length; i ++) {
+        w.push(c[i].length);
+        console.log(Math.max(...w));
+        
+        // console.log(w);
+        
+    }
+    // console.log(w);
+    
+}
+
+findLongestWord(c)
